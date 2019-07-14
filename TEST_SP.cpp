1ee3516f4d4798e021559038adcc95e3966738df
Grant Bishop <bishopgrant@seattleu.edu>
2019-07-13 19:33:47 -0700

    Small test to show segfault

---
+++ TEST_SP.cpp
@@ -0,0 +1,49 @@
+// TODO Header
+//
+
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <utility>
+
+#include "db_cxx.h"
+
+#include "storage_engine.h"
+#include "heap_storage.h"
+
+// Silence some issues w/ including heap_storage
+DbEnv* _DB_ENV;
+
+int main() {
+
+    // Test data strings
+    std::string test_strings[] {
+        "lorem",
+        "ipsum",
+        "dolor"
+    };
+    const size_t NUM_TEST_DATAS = 3;
+
+    // Test data dbt structs
+    Dbt test_data_dbts[NUM_TEST_DATAS];
+    for(size_t i = 0; i < NUM_TEST_DATAS; ++i) {
+        void* bits_i = (void*)(test_strings[i].c_str()); 
+        test_data_dbts[i] = Dbt(bits_i,sizeof(bits_i));
+    }
+
+    // Make sure Dbts constructed well
+    std::cout << "Data in a Dbt: " << (char*)test_data_dbts[2].get_data() << std::endl;
+
+    u_int32_t blockID = 1;
+
+    // Create a sloted page
+    Dbt _sp_1_backing;
+    SlottedPage test_spage_1(_sp_1_backing,blockID);
+
+    // Add to slotted page
+    test_spage_1.add(&test_data_dbts[0]);
+    test_spage_1.add(&test_data_dbts[1]);
+
+
+}
